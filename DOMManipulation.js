93cfd447038a3d26ce5db7dbdad4665a2eb9b2a3
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-09 17:07:13 +0300

    Add files via upload

--- DOMManipulation.js
+++ DOMManipulation.js
@@ -4,14 +4,27 @@
 const content = document.querySelector('.content');
 const div = document.querySelector('div');
 const titles = document.querySelectorAll('h1');
+const btn = document.querySelector('.btn');
 
 console.log(div.classList);
 
-div.classList.add('custom')
+//div.classList.add('custom')
 
+btn.addEventListener('click', () => {
+    div.classList.toggle('custom');
+    console.log(div.className)
+})
+console.log(div.classList.contains('custom'));
 
+div.setAttribute('id', "myId");
+console.log(div.getAttribute('id'));
+console.log(div.hasAttribute('id'));
 
+div.removeAttribute('id');
+console.log(div.hasAttribute('id'));
 
+console.log(div.dataset)
+console.log(div.dataset.myattr)
 
 
 
