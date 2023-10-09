2dce65d8f35ea43f89920d5d86133b89ae19aa55
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-09 14:29:37 +0300

    Add files via upload

--- DOMManipulation.js
+++ DOMManipulation.js
@@ -1,16 +1,13 @@
 
 
 
-const container = document.querySelector('.content');
+const content = document.querySelector('.content');
 const div = document.querySelector('div');
 const titles = document.querySelectorAll('h1');
-console.dir(titles);
-//console.log(Array.from(titles));
-console.log([...titles]);
-console.log(div.nextSibling);
-console.log(div.nextElementSibling);
-//console.log(div.firstChild);
-//console.log(div.firstElementChild);
+
+console.log(div.classList);
+
+div.classList.add('custom')
 
 
 
